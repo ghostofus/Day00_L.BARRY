8043fa644da63ebcb3307fea79ac6da06834da8a
ghostofus <148325055+ghostofus@users.noreply.github.com>
2024-05-13 12:55:34 +0200

    Ajout ex05
    Ex05 complété

---
+++ ex05/ex05.py
@@ -0,0 +1,28 @@
+import random
+
+def tri_insertion_decroissant(tab):
+    for i in range(1, len(tab)):
+        valeur_a_inserer = tab[i]
+        position = i
+
+        # Déplacement des éléments plus petits que la valeur à insérer vers la droite
+        while position > 0 and tab[position - 1] < valeur_a_inserer:
+            tab[position] = tab[position - 1]
+            position -= 1
+
+        # Insérer la valeur à sa position correcte
+        tab[position] = valeur_a_inserer
+
+
+def generer_tableau_aleatoire(taille):
+    return [random.randint(1, 100) for _ in range(taille)]
+
+
+tableau = generer_tableau_aleatoire(20)
+
+print("Tableau initial :", tableau)
+
+
+tri_insertion_decroissant(tableau)
+
+print("Tableau trié par ordre décroissant :", tableau)
