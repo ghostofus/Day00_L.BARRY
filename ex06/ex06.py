9217dc8fb1deaed749b1b305c037218e38c578e9
ghostofus <148325055+ghostofus@users.noreply.github.com>
2024-05-13 13:06:39 +0200

    Ajout ex06
    ex06 complété

---
+++ ex06/ex06.py
@@ -0,0 +1,7 @@
+def nombre_caracteres(chaine):
+    return len(chaine)
+
+# Test de la fonction
+chaine_test = "Bonjour, monde !"
+resultat = nombre_caracteres(chaine_test)
+print("Nombre de caractères dans la chaîne :", resultat)
